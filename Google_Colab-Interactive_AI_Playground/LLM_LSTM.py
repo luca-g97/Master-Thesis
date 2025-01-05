620bb9521deae9add8575802dcaaed35417e9697
luca-g97 <lg-gaming@web.de>
2025-01-05 13:48:00 +0100

    Adjusted LSTM to work with sentences instead of sequences in hook-mode

--- Google_Colab-Interactive_AI_Playground/LLM_LSTM.py
+++ Google_Colab-Interactive_AI_Playground/LLM_LSTM.py
@@ -491,7 +491,8 @@ def visualize(hidden_sizes, closestSources, showClosestMostUsedSources, visualiz
     generatedEvalSentences = [split_data(generatedEvalSentence, 2)[0][1] for generatedEvalSentence in generatedEvals]
 
     # Split the combined sentences into sentences and words
-    eval_source_structure = [create_sequences(generatedEvalSentences)[1]]
+    #eval_source_structure = [create_sequences(generatedEvalSentences)[1]] #For sequences
+    eval_source_structure = [create_sequences(generatedEvalSentences)] #For sentence
     sentences, words = split_data(" ".join(generatedEvalSentences))
     generatedEvalLoader = prepare_data_loader(sentences, words, seq_len=seq_len, batch_size=1, shuffle=False)
     RENN.initializeEvaluationHook(hidden_sizes, generatedEvalLoader, len(generatedEvalLoader), model, os.path.join("Evaluation", "Generated"), True, 0, True)
